1baf247b28698ae5d2b903f820d3eb3a28d20ba0
zhaolu <1632089314@qq.com>
2016-08-05 00:52:28 +0800

    toggle editor preview

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 const App = React.createClass({
+  getInitialState: function () {
+    return {
+      isEditor: true,
+      elements: []
+    }
+  },
+  toggle: function () {
+    this.setState({
+      isEditor: !this.state.isEditor
+    });
+  },
   render: function () {
-    return <div>hello</div>
+    return <div>
+      <button onClick={this.toggle}>{this.state.isEditor ? 'Preview' : 'Edit'}</button>
+      <div className={this.state.isEditor ? '' : 'hidden'}><Editor /></div>
+      <div className={this.state.isEditor ? 'hidden' : ''}><Preview /></div>
+    </div>
+  }
+});
+
+const Editor = React.createClass({
+  render: function(){
+    return <div>Editor</div>
+  }
+});
+
+const Preview = React.createClass({
+  render: function(){
+    return <div>Preview</div>
   }
 });
 
